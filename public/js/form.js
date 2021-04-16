11e5b4fe5ac796e923eab6e57067da1a43664774
kiwumulo nickson <nicksonk2017@gmail.com>
2021-04-16 08:09:12 +0300

    third commit

--- public/js/form.js
+++ public/js/form.js
@@ -1,11 +1,11 @@
 // Accessing elements in the HTML form
 const regform = document.getElementById('form');
 const uname=document.getElementById('username');
-const upass=document.getElementById('password');
+const upast=document.getElementById('pi');
 const uemail=document.getElementById('email');
 //Acessing errors
 const nameError= document.getElementById ('nameErr')
-const passError= document.getElementById ('passErr')
+const pastError= document.getElementById ('piErr')
 const emailError= document.getElementById ('emailErr')
  // Add eventlistener to the form
  regform.addEventListener('submit', (event)=>{
@@ -32,42 +32,42 @@ const validate = ()=>{
     };
 
 
-    let alphanumeric = /^(?=.*[a-zA-Z])(?=.*[0-9])[A-Za-z0-9]+$/;
-    if(upass.value==''){
-        passError.innerHTML='Field Required';
-        passError.style.color='red';
-        upass.style.border='1px solid red';
-        return false
-    }
-    else if(!upass.value.match(alphanumeric)){
-        passError.innerHTML='Username  should be a mixture of numbers & letters';
-        passError.style.color='red';
-        upass.style.border='1px solid red';
-        return false
+//     let alphanumeric = /^(?=.*[a-zA-Z])(?=.*[0-9])[A-Za-z0-9]+$/;
+//     if(upass.value==''){
+//         passError.innerHTML='Field Required';
+//         passError.style.color='red';
+//         upass.style.border='1px solid red';
+//         return false
+//     }
+//     else if(!upass.value.match(alphanumeric)){
+//         passError.innerHTML='Username  should be a mixture of numbers & letters';
+//         passError.style.color='red';
+//         upass.style.border='1px solid red';
+//         return false
         
-    }
-    else if (upass.value.match(alphanumeric)){
-        upass.style.border='1px solid green';
+//     }
+//     else if (upass.value.match(alphanumeric)){
+//         upass.style.border='1px solid green';
     
-    };
-    // let text = /^[A-Za-z]+$/
-    let Regex5=/^\d{1}$/;
-    if(uemail.value==''){
-       emailError.innerHTML='Field Required';
-       emailError.style.color='red';
-       uemail.style.border='1px solid red';
-       return false
-   }
-   else if(!uemail.value.match(Regex5)){
-       emailError.innerHTML='Ward No should be one digit ';
-       emailError.style.color='red';
-       uemail.style.border='1px solid red';
-       return false
-   }
-   else if (uemail.value.match(Regex5)){
-    uemail.style.border='1px solid green';
+//     };
+//     // let text = /^[A-Za-z]+$/
+//     let Regex5=/^\d{1}$/;
+//     if(uemail.value==''){
+//        emailError.innerHTML='Field Required';
+//        emailError.style.color='red';
+//        uemail.style.border='1px solid red';
+//        return false
+//    }
+//    else if(!uemail.value.match(Regex5)){
+//        emailError.innerHTML='Ward No should be one digit ';
+//        emailError.style.color='red';
+//        uemail.style.border='1px solid red';
+//        return false
+//    }
+//    else if (uemail.value.match(Regex5)){
+//     uemail.style.border='1px solid green';
 
-};
+// };
 
 
 
